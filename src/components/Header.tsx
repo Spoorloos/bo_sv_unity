b703e8b2d0f30e7fba81c2a723cdbe9b45682e85
Mick Negenman <36791@ma-web.nl>
2024-09-30 12:38:05 +0200

    header mobile support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,16 +18,18 @@ export default function Header() {
 
     return (
         <header className="mx-8 my-4 pb-2 flex items-center justify-between border-b-2 border-gray-300">
-            <Image src="/logo.svg" alt="logo" width="67" height="62" priority/>
-            {navEnabled && <nav className="flex gap-4 font-kinetika">
-                {Object.entries(tabs).map(([ name, url ], index) =>
-                    <Link className={`transition-colors border-b-2 ${url === pathName ? "border-accent text-black" : "border-transparent text-gray-500 hover:text-black"} p-2 uppercase`} href={url} key={index}>{name}</Link>
+            <Link href="/">
+                <Image src="/logo.svg" alt="logo" width="67" height="62" priority/>
+            </Link>
+            <nav className={`${navEnabled ? 'flex bg-white fixed inset-0 flex-col items-start pt-24 p-[10%] text-3xl' : 'hidden sm:flex'} gap-4 font-kinetika`}>
+                {Object.entries(tabs).map(([name, url], index) =>
+                    <Link className={`transition-colors border-b-2 ${url === pathName ? "border-accent text-black" : "border-transparent text-gray-500 hover:text-black"} p-2 uppercase`} href={url} key={index} onClick={() => setNavEnabled(false)}>{name}</Link>
                 )}
-            </nav>}
-            <button className="w-12 h-10 relative" onClick={() => setNavEnabled(!navEnabled)}>
-                <div className={`transition-all duration-300 w-full h-1 bg-black absolute ${navEnabled ? 'top-1/2 -translate-y-1/2 rotate-45' : 'top-0'}`}></div>
-                <div className={`transition-all duration-300 w-full h-1 bg-black absolute top-1/2 -translate-y-1/2 ${navEnabled ? 'opacity-0' : ''}`}></div>
-                <div className={`transition-all duration-300 w-full h-1 bg-black absolute ${navEnabled ? 'top-1/2 -translate-y-1/2 -rotate-45' : 'top-full -translate-y-full'}`}></div>
+            </nav>
+            <button className="w-12 h-10 relative sm:hidden" onClick={() => setNavEnabled(!navEnabled)}>
+                <div className={`transition-all duration-300 w-full h-1 rounded bg-black absolute ${navEnabled ? 'top-1/2 -translate-y-1/2 rotate-45' : 'top-0'}`}></div>
+                <div className={`transition-all duration-300 w-full h-1 rounded bg-black absolute top-1/2 -translate-y-1/2 ${navEnabled ? 'opacity-0' : ''}`}></div>
+                <div className={`transition-all duration-300 w-full h-1 rounded bg-black absolute ${navEnabled ? 'top-1/2 -translate-y-1/2 -rotate-45' : 'top-full -translate-y-full'}`}></div>
             </button>
         </header>
     );

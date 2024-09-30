3ba5124acb0366576481a20cbd82ddbffde2c9d0
Mick Negenman <36791@ma-web.nl>
2024-09-30 12:42:01 +0200

    clean up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,16 +14,26 @@ const tabs = {
 
 export default function Header() {
     const pathName = usePathname();
-    const [ navEnabled, setNavEnabled ] = useState(false);
+    const [navEnabled, setNavEnabled] = useState(false);
 
     return (
         <header className="mx-8 my-4 pb-2 flex items-center justify-between border-b-2 border-gray-300">
             <Link href="/">
-                <Image src="/logo.svg" alt="logo" width="67" height="62" priority/>
+                <Image
+                    src="/logo.svg"
+                    alt="logo"
+                    width="67"
+                    height="62"
+                    priority/>
             </Link>
-            <nav className={`${navEnabled ? 'flex bg-white fixed inset-0 flex-col items-start pt-24 p-[10%] text-3xl' : 'hidden sm:flex'} gap-4 font-kinetika`}>
+            <nav className={`gap-4 font-kinetika ${navEnabled ? 'flex bg-white fixed inset-0 flex-col items-start pt-24 p-[10%] text-3xl' : 'hidden sm:flex'}`}>
                 {Object.entries(tabs).map(([name, url], index) =>
-                    <Link className={`transition-colors border-b-2 ${url === pathName ? "border-accent text-black" : "border-transparent text-gray-500 hover:text-black"} p-2 uppercase`} href={url} key={index} onClick={() => setNavEnabled(false)}>{name}</Link>
+                    <Link
+                        className={`transition-colors border-b-2 p-2 uppercase ${url === pathName ? "border-accent text-black" : "border-transparent text-gray-500 hover:text-black"}`}
+                        href={url}
+                        key={index}
+                        onClick={() => setNavEnabled(false)}
+                    >{name}</Link>
                 )}
             </nav>
             <button className="w-12 h-10 relative sm:hidden" onClick={() => setNavEnabled(!navEnabled)}>

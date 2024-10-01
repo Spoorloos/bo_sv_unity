dab16f4614a671eed7e914c4b49cba40068d54ce
Mick Negenman <36791@ma-web.nl>
2024-10-01 12:50:11 +0200

    fixed bug with menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from "@/components/Logo";
 import Link from "next/link";
 import localFont from "next/font/local";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tabs = {
     "Home": "/",
@@ -20,27 +20,31 @@ const kinetika = localFont({
 export default function Header() {
     const pathName = usePathname();
     const [navEnabled, setNavEnabled] = useState(false);
+    const [animate, setAnimate] = useState(false);
 
-    useEffect(() => {
+    function toggleNavBar() {
+        setNavEnabled(!navEnabled);
+        setAnimate(true);
+        setTimeout(() => setAnimate(false), 300);
         document.body.classList.toggle("overflow-hidden", navEnabled);
-    }, [navEnabled]);
+    }
 
     return (
         <header className="mb-4 pb-2 flex items-center justify-between border-b-2 border-border">
             <Link href="/">
                 <Logo className="h-16 w-auto text-[#313131] dark:text-gray-300" textColor="currentColor"/>
             </Link>
-            <nav className={`transition-transform origin-top fixed inset-0 bg-background flex flex-col items-start gap-4 p-[10%] pt-24 text-3xl ${kinetika.className} ${navEnabled ? "scale-y-100" : "scale-y-0"}`}>
+            <nav className={`origin-top fixed sm:static inset-0 bg-background flex flex-col sm:flex-row items-start gap-4 p-[10%] pt-24 sm:p-0 text-3xl sm:text-base sm:transform-none ${kinetika.className} ${animate ? "transition-transform duration-300" : ""} ${navEnabled ? "scale-y-100" : "scale-y-0"}`}>
                 {Object.entries(tabs).map(([name, url], index) =>
                     <Link
                         className={`transition-opacity border-b-2 p-2 uppercase ${url === pathName ? "border-accent" : "border-transparent opacity-50 hover:opacity-100"}`}
                         href={url}
                         key={index}
-                        onClick={() => setNavEnabled(false)}
+                        onClick={toggleNavBar}
                     >{name}</Link>
                 )}
             </nav>
-            <button className="w-10 h-8 relative sm:hidden z-10" onClick={() => setNavEnabled(!navEnabled)}>
+            <button className="w-10 h-8 relative sm:hidden z-10" onClick={toggleNavBar}>
                 <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute ${navEnabled ? "top-1/2 -translate-y-1/2 rotate-45" : "top-0"}`}></div>
                 <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute top-1/2 -translate-y-1/2 ${navEnabled ? "opacity-0" : ""}`}></div>
                 <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute ${navEnabled ? "top-1/2 -translate-y-1/2 -rotate-45" : "top-full -translate-y-full"}`}></div>

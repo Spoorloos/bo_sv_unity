3f728be0abcf80fb69b5418f25a5f78bb4e39c79
Mick Negenman <36791@ma-web.nl>
2024-10-02 20:35:21 +0200

    added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="flex justify-center items-center gap-4 absolute inset-0 -z-10">
+            <span className="text-3xl pe-4 border-e border-border">404</span>
+            <span className="text-xl font-thin">This page could not be found</span>
+        </div>
+    )
+}

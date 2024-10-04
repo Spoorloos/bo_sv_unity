fa54d1e7597eb01bd1c338200e05a2a6546510e0
Mick Negenman <36791@ma-web.nl>
2024-10-04 17:49:03 +0200

    minor improvement

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -5,10 +5,10 @@ type Hamburger = Readonly<{
 
 export default function Hamburger({ toggled, onToggle }: Hamburger) {
     return (
-        <button className="w-10 h-8 relative sm:hidden z-10" onClick={onToggle}>
-            <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute ${toggled ? "top-1/2 -translate-y-1/2 rotate-45" : "top-0"}`}></div>
-            <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute top-1/2 -translate-y-1/2 ${toggled ? "opacity-0" : ""}`}></div>
-            <div className={`transition-all duration-300 w-full h-1 rounded bg-current absolute ${toggled ? "top-1/2 -translate-y-1/2 -rotate-45" : "top-full -translate-y-full"}`}></div>
+        <button className="w-10 h-8 relative sm:hidden z-10 *:transition-all *:duration-300 *:w-full *:h-1 *:rounded *:bg-current *:absolute" onClick={onToggle}>
+            <div className={toggled ? "top-1/2 -translate-y-1/2 rotate-45" : "top-0"}></div>
+            <div className={`top-1/2 -translate-y-1/2 ${toggled ? "opacity-0" : ""}`}></div>
+            <div className={toggled ? "top-1/2 -translate-y-1/2 -rotate-45" : "top-full -translate-y-full"}></div>
         </button>
     )
 }

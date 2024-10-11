f277d280d68774d0bad878e2e55f37affb403f03
Mick Negenman <36791@ma-web.nl>
2024-10-11 14:09:00 +0200

    Minor improvements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,28 +7,28 @@ import Telegram from "@/public/telegram.svg";
 
 export default function Footer() {
     return (
-        <footer className="px-[10%] pt-0 pb-3">
+        <footer className="px-[10%] pb-3">
             <div className="mb-3 p-8 border-b-2 border-border grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
                 <ul className="space-y-2 text-start sm:text-start">
                     <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 1</li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum dolor.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem.</a></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
                 <ul className="space-y-2 text-start sm:text-end lg:text-start">
                     <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 2</li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum dolor.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem.</a></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
                 <ul className="space-y-2 text-start sm:text-start">
                     <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 3</li>
-                    <li className="accent-hover"><a href="#">Lorem.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum.</a></li>
-                    <li className="accent-hover"><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><Link className="accent-hover" href="#">Lorem.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
+                    <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                 </ul>
                 <div className="flex flex-col justify-between text-start sm:text-end lg:text-start gap-8 sm:gap-0">
                     <section className="flex flex-col justify-between gap-2">
@@ -41,11 +41,11 @@ export default function Footer() {
                     <section className="flex flex-col justify-between gap-2">
                         <h2 className="font-bold font-kinetika uppercase text-1xl">Connect With Us</h2>
                         <div className="h-8 flex gap-3 *:h-full *:*:h-full justify-start sm:justify-end lg:justify-start">
-                            <Link className="accent-hover" href="#"><Instagram/></Link>
-                            <Link className="accent-hover" href="#"><Youtube/></Link>
-                            <Link className="accent-hover" href="#"><Facebook/></Link>
-                            <Link className="accent-hover" href="#"><Twitter/></Link>
-                            <Link className="accent-hover" href="#"><Telegram/></Link>
+                            <Link href="#"><Instagram className="accent-hover"/></Link>
+                            <Link href="#"><Youtube className="accent-hover"/></Link>
+                            <Link href="#"><Facebook className="accent-hover"/></Link>
+                            <Link href="#"><Twitter className="accent-hover"/></Link>
+                            <Link href="#"><Telegram className="accent-hover"/></Link>
                         </div>
                     </section>
                 </div>

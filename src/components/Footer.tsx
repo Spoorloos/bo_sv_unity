c2cfbd81595240c65dff9f5d62959afd7a7dd0ee
Mick Negenman <36791@ma-web.nl>
2024-10-11 14:35:33 +0200

    Minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,21 +10,21 @@ export default function Footer() {
         <footer className="px-[10%] pb-3">
             <div className="mb-3 p-8 border-b-2 border-border grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
                 <ul className="space-y-2 text-start sm:text-start">
-                    <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 1</li>
+                    <li className="footer-column">Column 1</li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
                 <ul className="space-y-2 text-start sm:text-end lg:text-start">
-                    <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 2</li>
+                    <li className="footer-column">Column 2</li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
                 <ul className="space-y-2 text-start sm:text-start">
-                    <li className="font-bold font-kinetika uppercase text-lg mb-2">Column 3</li>
+                    <li className="footer-column">Column 3</li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
@@ -32,14 +32,14 @@ export default function Footer() {
                 </ul>
                 <div className="flex flex-col justify-between text-start sm:text-end lg:text-start gap-8 sm:gap-0">
                     <section className="flex flex-col justify-between gap-2">
-                        <h2 className="font-bold font-kinetika uppercase text-1xl">Newsletter</h2>
+                        <h2 className="footer-column">Newsletter</h2>
                         <form className="flex" action="https://youtube.com">
                             <input className="w-full p-2 border border-border bg-transparent outline-none" type="email" name="email" placeholder="Email address" required/>
                             <button className="p-2 transition-colors text-background bg-gray-800 dark:bg-gray-400 hover:bg-accent font-kinetika uppercase" type="submit">Sub</button>
                         </form>
                     </section>
                     <section className="flex flex-col justify-between gap-2">
-                        <h2 className="font-bold font-kinetika uppercase text-1xl">Connect With Us</h2>
+                        <h2 className="footer-column">Connect With Us</h2>
                         <div className="h-8 flex gap-3 *:h-full *:*:h-full justify-start sm:justify-end lg:justify-start">
                             <Link href="#"><Instagram className="accent-hover"/></Link>
                             <Link href="#"><Youtube className="accent-hover"/></Link>

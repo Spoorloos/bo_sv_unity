a328c948ccecdd5770f2ad57c7127b940a9d13e9
Mick Negenman <36791@ma-web.nl>
2024-10-11 14:28:11 +0200

    Changed hamburger divs to spans because block elements aren't allowed inside buttons

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -5,10 +5,10 @@ type Hamburger = Readonly<{
 
 export default function Hamburger({ toggled, onToggle }: Hamburger) {
     return (
-        <button className="w-10 h-8 relative sm:hidden z-10 *:transition-all *:duration-300 *:w-full *:h-1 *:rounded *:bg-current *:absolute" onClick={onToggle} tabIndex={1}>
-            <div className={toggled ? "top-1/2 -translate-y-1/2 rotate-45" : "top-0"}></div>
-            <div className={`top-1/2 -translate-y-1/2 ${toggled ? "opacity-0" : ""}`}></div>
-            <div className={toggled ? "top-1/2 -translate-y-1/2 -rotate-45" : "top-full -translate-y-full"}></div>
+        <button className="w-10 h-8 relative sm:hidden z-10" onClick={onToggle} tabIndex={1}>
+            <span className={`hamburger-line ${toggled ? "top-1/2 -translate-y-1/2 rotate-45" : "top-0"}`}/>
+            <span className={`hamburger-line top-1/2 -translate-y-1/2 ${toggled ? "opacity-0" : ""}`}/>
+            <span className={`hamburger-line ${toggled ? "top-1/2 -translate-y-1/2 -rotate-45" : "top-full -translate-y-full"}`}/>
         </button>
     )
 }

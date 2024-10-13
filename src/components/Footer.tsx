27d60d06b96e93b8682c87af69cf69a19deb10fc
Mick Negenman <36791@ma-web.nl>
2024-10-13 16:05:57 +0200

    Minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,28 +8,28 @@ export default function Footer() {
     return (
         <footer className="px-[10%]">
             <div className="px-[5%] pb-6 border-b-2 border-border grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-                <ul className="space-y-2 text-start sm:text-start">
+                <ul className="space-y-2">
                     <li className="footer-column">Column 1</li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
-                <ul className="space-y-2 text-start sm:text-end lg:text-start">
+                <ul className="space-y-2 sm:text-end lg:text-start">
                     <li className="footer-column">Column 2</li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                 </ul>
-                <ul className="space-y-2 text-start sm:text-start">
+                <ul className="space-y-2">
                     <li className="footer-column">Column 3</li>
                     <li><Link className="accent-hover" href="#">Lorem.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum.</Link></li>
                     <li><Link className="accent-hover" href="#">Lorem, ipsum dolor.</Link></li>
                 </ul>
-                <div className="grid text-start sm:text-end lg:text-start">
+                <div className="grid sm:text-end lg:text-start">
                     <section className="space-y-2">
                         <h2 className="footer-column">Nieuwsbrief</h2>
                         <form className="flex" action="https://youtube.com">
@@ -39,11 +39,11 @@ export default function Footer() {
                     </section>
                     <section className="space-y-2 pt-8 sm:pt-0 mt-auto">
                         <h2 className="footer-column">Socials</h2>
-                        <div className="h-7 space-x-3 *:h-full *:*:h-full sm:text-end lg:text-start">
-                            <Link href="#"><Instagram className="inline accent-hover"/></Link>
-                            <Link href="#"><Youtube className="inline accent-hover"/></Link>
-                            <Link href="#"><Facebook className="inline accent-hover"/></Link>
-                            <Link href="#"><Twitter className="inline accent-hover"/></Link>
+                        <div className="h-7 space-x-3">
+                            <Link className="h-full" href="#"><Instagram className="inline h-full accent-hover"/></Link>
+                            <Link className="h-full" href="#"><Youtube className="inline h-full accent-hover"/></Link>
+                            <Link className="h-full" href="#"><Facebook className="inline h-full accent-hover"/></Link>
+                            <Link className="h-full" href="#"><Twitter className="inline h-full accent-hover"/></Link>
                         </div>
                     </section>
                 </div>

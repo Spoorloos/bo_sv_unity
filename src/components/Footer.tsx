572381ebee8e65e16ac01c6542340558c339fd8d
Mick Negenman <36791@ma-web.nl>
2024-10-14 09:14:01 +0200

    Minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,9 +35,9 @@ const socials = {
 export default function Footer() {
     return (
         <footer className="px-[10%]">
-            <div className="px-[5%] pb-6 border-b-2 border-border grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="px-[5%] pb-8 border-b-2 border-border grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
                 {Object.entries(links).map(([title, items], index) =>
-                    <section key={index}>
+                    <section key={index} className={index % 2 === 1 ? "sm:text-end lg:text-start" : ""}>
                         <h2 className="footer-column mb-2">{title}</h2>
                         <ul className="space-y-2">
                             {Object.entries(items).map(([name, url], index) =>
@@ -51,16 +51,16 @@ export default function Footer() {
                         <h2 className="footer-column">Nieuwsbrief</h2>
                         <form className="flex news-letter" method="POST" action="/nieuwsbrief">
                             <input className="w-full p-2 border border-r-0 bg-transparent outline-none rounded-none transition-colors duration-100 border-[--color]" type="email" name="email" placeholder="Jouw e-mailadres" required/>
-                            <button className="p-2 text-background font-kinetika uppercase transition-colors duration-100 bg-[--color]" type="submit" aria-label="Abonneer op nieuwsbrief">Sub</button>
+                            <button className="p-2 text-background font-kinetika uppercase transition-colors duration-100 bg-[--color]" type="submit" aria-label="Abonneer op nieuwsbrief knop" title="Abonneer op onze nieuwsbrief">Sub</button>
                         </form>
                     </section>
-                    <section className="space-y-2 pt-8 sm:pt-0 mt-auto">
+                    <section className="space-y-2 pt-6 sm:pt-0 mt-auto">
                         <h2 className="footer-column">Socials</h2>
                         <div className="h-7 space-x-3">
-                            <Link className="h-full inline-block accent-hover" href={socials.instagram} aria-label="Instagram"><Instagram className="inline h-full"/></Link>
-                            <Link className="h-full inline-block accent-hover" href={socials.youtube} aria-label="Youtube"><Youtube className="inline h-full"/></Link>
-                            <Link className="h-full inline-block accent-hover" href={socials.facebook} aria-label="Facebook"><Facebook className="inline h-full"/></Link>
-                            <Link className="h-full inline-block accent-hover" href={socials.twitter} aria-label="Twitter"><Twitter className="inline h-full"/></Link>
+                            <Link className="h-full inline-block accent-hover" href={socials.instagram} aria-label="Instagram" title="Instagram"><Instagram className="inline h-full"/></Link>
+                            <Link className="h-full inline-block accent-hover" href={socials.youtube} aria-label="Youtube" title="Youtube"><Youtube className="inline h-full"/></Link>
+                            <Link className="h-full inline-block accent-hover" href={socials.facebook} aria-label="Facebook" title="Facebook"><Facebook className="inline h-full"/></Link>
+                            <Link className="h-full inline-block accent-hover" href={socials.twitter} aria-label="Twitter" title="Twitter"><Twitter className="inline h-full"/></Link>
                         </div>
                     </section>
                 </div>

6591f69858b400ed68f1db7acd510ba80d619436
Mick Negenman <36791@ma-web.nl>
2024-10-23 15:13:28 +0200

    Added link to homepage on logo

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -9,7 +9,7 @@ type NavItem = Readonly<{
 
 export default function NavItem({ name, href, selected, onClick }: NavItem) {
     return (
-        <Link className="uppercase block sm:inline" href={href} onClick={onClick} aria-label={`${name} pagina`} role="none" scroll={false}>
+        <Link className="uppercase block sm:inline" href={href} onClick={onClick} aria-label={`${name} pagina`} scroll={false}>
             <span className={`transition-colors p-2 inline-block border-b-2 text-text text-3xl sm:text-base font-kinetika ${selected ? "border-accent" : "border-transparent text-opacity-50 hocus:text-opacity-100"}`}>{name}</span>
         </Link>
     )

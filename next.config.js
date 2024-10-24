212e30999eff75110101d21040ef4151559b8b97
Mick Negenman <36791@ma-web.nl>
2024-10-24 09:35:08 +0200

    Bug fix

--- next.config.js
+++ next.config.js
@@ -13,20 +13,13 @@ export default {
     },
     webpack: (config) => {
         const fileLoaderRule = config.module.rules.find(
-            (rule) => rule.test?.test?.(".svg")
+            rule => rule.test?.test?.(".svg")
         );
 
         config.module.rules.push({
             ...fileLoaderRule,
             test: /\.svg$/i,
-            resourceQuery: /url/,
-        }, {
-            test: /\.svg$/i,
-            issuer: fileLoaderRule.issuer,
-            resourceQuery: {
-                not: [...fileLoaderRule.resourceQuery.not, /url/]
-            },
-            use: ["@svgr/webpack"],
+            loader: "@svgr/webpack",
         });
 
         fileLoaderRule.exclude = /\.svg$/i;

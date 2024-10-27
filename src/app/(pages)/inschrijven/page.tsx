b0174f7515d134f209b778f051aced9b9081327a
Spoorloos <negenmanmick@gmail.com>
2024-10-27 18:27:24 +0100

    More HTML error fixes

--- src/app/(pages)/inschrijven/page.tsx
+++ src/app/(pages)/inschrijven/page.tsx
@@ -1,22 +1,22 @@
-export default function Signup() {
+export default function SignUp() {
     return (
         <main className="absolute px-8 py-3 inset-0 flex flex-col justify-center items-center">
             <h1 className="my-8 text-5xl font-bold font-kinetika">Inschrijven</h1>
-            <form className="w-full max-w-96 space-y-3" action="/" method="POST">
-                <div>
-                    <label className="text-xl font-thin block mb-1" htmlFor="naam">Naam</label>
+            <form className="w-full max-w-sm space-y-3" action="/" method="POST">
+                <fieldset>
+                    <legend className="text-xl font-thin block mb-1">Naam</legend>
                     <div className="flex flex-wrap gap-3">
-                        <input className="textfield min-w-36 w-full flex-1" type="text" name="first_name" placeholder="Voornaam" required/>
-                        <input className="textfield min-w-36 w-full flex-1" type="text" name="last_name" placeholder="Achternaam" required/>
+                        <input className="textfield flex-1 w-full min-w-[min(100%,9rem)]" type="text" name="first_name" placeholder="Voornaam" required/>
+                        <input className="textfield flex-1 w-full min-w-[min(100%,9rem)]" type="text" name="last_name" placeholder="Achternaam" required/>
                     </div>
-                </div>
+                </fieldset>
                 <div>
                     <label className="text-xl font-thin block mb-1" htmlFor="email">E-mail</label>
-                    <input className="textfield w-full" type="email" name="email" placeholder="email@voorbeeld.nl" required/>
+                    <input className="textfield w-full" type="email" name="email" id="email" placeholder="email@voorbeeld.nl" required/>
                 </div>
                 <div>
                     <label className="text-xl font-thin block mb-1" htmlFor="birthday">Geboortedag</label>
-                    <input className="textfield w-full" type="date" name="birthday" required/>
+                    <input className="textfield w-full" type="date" name="birthday" id="birthday" required/>
                 </div>
                 <div className="flex justify-end">
                     <button className="accent-button rounded-lg" type="submit">Schrijf je in</button>

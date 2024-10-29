7002da7e7ee97a69ef34bf4d7321c6c9f7f313b3
Mick Negenman <36791@ma-web.nl>
2024-10-29 19:36:28 +0100

    Shorter text on hero section because it looks way better on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
     return (
         <header className="pb-3 flex items-center justify-between border-b-2 border-border z-10">
             <Link href="/" aria-label="Home pagina" scroll={false}>
-                <Logo className="h-16 w-auto text-[#313131] dark:text-[#D1D5DB]"/>
+                <Logo className="h-16 w-auto text-[#D1D5DB]"/>
             </Link>
             <nav className={`origin-top fixed sm:static inset-0 bg-background space-y-4 sm:space-y-0 sm:space-x-4 p-[10%] pt-24 sm:p-0 z-40 ${navEnabled ? "scale-y-100 opacity-100 open" : "scale-y-0 sm:transform-none opacity-0 sm:opacity-100 invisible sm:visible"} ${animate && "transition-all duration-300"}`}>
                 {Object.entries(tabs).map(([name, url], index) =>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Hero() {
     return (
         <section className="h-full overflow-auto flex flex-col justify-around items-center gap-8 md:flex-row">
-            <div className="text-center md:text-left max-w-md space-y-3">
+            <div className="text-center sm:text-left max-w-md space-y-3">
                 <h1 className="text-[min(17vw,5rem)] font-bold font-kinetika leading-none text-drop-accent">S.V. Unity</h1>
-                <p className="text-[min(7vw,1.25rem)] font-thin">De MBO studentenvereniging van Amsterdam, waar vriendschap, groei en onvergetelijke ervaringen samenkomen!</p>
+                <p className="text-[min(7vw,1.25rem)] font-thin">Gezelligheid, verbinding en belangen MBO studenten</p>
                 <Link className="accent-button" href="/inschrijven" scroll={false}>Schrijf je in</Link>
             </div>
             <div className="inline-block overflow-hidden max-h-[85%] aspect-[4/3]">
